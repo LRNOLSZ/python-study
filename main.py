35994dcb0de3d64c4db6a222355de62d34a30a4a
LRNOLSZ <laryea024@gmail.com>
2024-11-01 09:34:45 -0700

    recommiting

--- main.py
+++ main.py
@@ -2,3 +2,4 @@
 #this is a learnig progressive project python project
 #this is a third commit trial of main.py file
 #this is a fourth commit test of the py file using command line
+#this si the second test of saving with terminal
