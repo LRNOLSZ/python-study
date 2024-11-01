c7e90701acf89341a5fdfc3d61e3e2a8755bc062
LRNOLSZ <laryea024@gmail.com>
2024-11-01 09:31:02 -0700

    this is a second trial commit message using terminal

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 #this is my python studies project
 #this is a learnig progressive project python project
 #this is a third commit trial of main.py file
+#this is a fourth commit test of the py file using command line
