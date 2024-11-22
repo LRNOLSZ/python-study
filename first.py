16ae90aba762fe91d71d8befd4a84eec8d9d3b84
LRNOLSZ <laryea024@gmail.com>
2024-11-22 01:19:16 -0800

    middle name added

---
+++ first.py
@@ -0,0 +1,9 @@
+#console test
+
+first_name = input("Enter your first name: ") 
+middle_name = input("state your middle name")
+last_name = input("Enter your last name: ") 
+
+print(f"Hello, {first_name} {middle_name} {last_name}! its nce to see you")
+
+
